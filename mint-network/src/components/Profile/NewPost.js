006f90d441bdea77875bf3d7dd781a5cecfd5aba
Ruslan <northernnavy95@gmail.com>
2022-05-02 14:16:23 +0300

    created raw example to adding posts

--- mint-network/src/components/Profile/NewPost.js
+++ mint-network/src/components/Profile/NewPost.js
@@ -1,11 +1,21 @@
 import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { addPost, typeNewPost } from "../../redux/reducers/profileReducer";
 
-const NewPost = (props) => {
+const NewPost = () => {
+    const dispatch = useDispatch();
+    const newValue = useSelector((state) => state.profile.newPostText);
+    const posts = useSelector((state) => state.profile.posts);
+    let uiposts = posts.map((obj) => {
+        return <div>{obj.text}</div>
+    })
     return (
         <div>
-            <textarea value={props.text}></textarea>
-            <button>добавить пост</button>
-            <div>все посты</div>
+            <textarea value={newValue} onChange={(event) => {
+                dispatch(typeNewPost(event.target.value));
+            }}></textarea>
+            <button onClick={() => dispatch(addPost())}>добавить пост</button>
+            <div>{uiposts}</div>
         </div>
     )
 }
