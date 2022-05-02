66a1d4678657d050a73df6b1a0b362cdf27c983a
Ruslan <northernnavy95@gmail.com>
2022-05-02 14:12:51 +0300

    removed mapStateToProps

--- mint-network/src/App.js
+++ mint-network/src/App.js
@@ -2,12 +2,13 @@ import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Header from './components/Header';
 import Navbar from './components/Navbar';
-import Profile from './components/Profile';
+import Profile from './components/Profile/Profile';
 import Users from './components/Users';
 import Messages from './components/Messages';
 import Settings from './components/Settings';
 
 const App = () => {
+  // все селекторы провести здесь, и далее прокинуть пропсами. это облегчит работу 
   return (
     <BrowserRouter>
       <div id='appWrapper'>
