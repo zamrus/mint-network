9a890efd5bdd819f8dc4fab954503fd8a50621ca
Ruslan <northernnavy95@gmail.com>
2022-05-02 14:11:06 +0300

    added provider, removed container

--- mint-network/src/index.js
+++ mint-network/src/index.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import ReactDOMClient from 'react-dom/client';
-import App from './App';
+import { Provider } from 'react-redux';
+import store from './redux/store';
 import reportWebVitals from './reportWebVitals';
+import App from './App';
 
 const container = document.getElementById('root');
 const root = ReactDOMClient.createRoot(container);
-root.render(<App />);
+
+root.render(
+    <Provider store={store}>
+        <App/>
+    </Provider>
+);
 
 
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
