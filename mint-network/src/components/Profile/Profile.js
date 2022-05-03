b8137b6e1c65db5e4c4aef202751e7052cf4b68b
Ruslan <northernnavy95@gmail.com>
2022-05-03 15:29:05 +0300

    added data selection within profile component. Later it will be at App level

--- mint-network/src/components/Profile/Profile.js
+++ mint-network/src/components/Profile/Profile.js
@@ -1,13 +1,14 @@
 import React from "react";
 import ProfileInfo from "./ProfileInfo";
-
+import { useSelector } from "react-redux";
 import NewPost from "./NewPost";
 
 const Profile = () => {
+    let status = useSelector((state) => state.profile.profileStatus);
     return (
         <div>
             Banner
-            <ProfileInfo />
+            <ProfileInfo status={status}/>
             <NewPost />
 
         </div>
