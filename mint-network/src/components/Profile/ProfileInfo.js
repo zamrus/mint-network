d4fb84a022141e61daf72f7fb411d30d767beebe
Ruslan <northernnavy95@gmail.com>
2022-05-03 15:30:09 +0300

    added possibility to write some status. This status is changing by hooks

--- mint-network/src/components/Profile/ProfileInfo.js
+++ mint-network/src/components/Profile/ProfileInfo.js
@@ -1,9 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
+import { useDispatch  } from "react-redux";
+import { addStatus } from "../../redux/reducers/profileReducer";
 
-const ProfileInfo = () => {
+const ProfileInfo = (props) => {
+
+    let dispatch = useDispatch();
+
+    let [isTyping, setIsTyping] = useState(false);
+
+    let activateTyping = () => setIsTyping(true);
+
+    let deactivateTyping = () => {
+        setIsTyping(false);
+        dispatch(addStatus(currentStatus));
+    }
+
+    let [currentStatus, changeStatus] = useState(props.status);
+    
+    let onStatusChange = (event) => {
+        changeStatus(event.currentTarget.value);
+    }
+
     return (
         <div>
-            avatar + add status
+            <div>
+                <img src={''} width={''} height={''} /> avatar
+            </div>
+            {isTyping 
+                ? <input type={'text'} autoFocus onChange={onStatusChange} onBlur={deactivateTyping} value={currentStatus} />
+                : <span onClick={activateTyping}>{props.status || '------------'}</span>
+            }
         </div>
     )
 }
