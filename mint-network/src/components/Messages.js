2cbc689c521a700cf807a702bde3888983b4a3a3
Ruslan <northernnavy95@gmail.com>
2022-05-06 20:07:15 +0300

    added possibility to write and send simple messages with UI

--- mint-network/src/components/Messages.js
+++ mint-network/src/components/Messages.js
@@ -1,12 +1,28 @@
 import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { sendMessage, typeNewMessage } from "../redux/reducers/messagesReducer";
 
 const Messages = () => {
+    let dispatch = useDispatch();
+    let message = useSelector((state) => state.messages.newMessageText);
+    let msg = useSelector((state) => state.messages.messages);
+    let uimessages = msg.map((obj) => {
+        return <div>{obj.text}</div>
+    })
+    
     return (
         <div>
             Messages info
             <div>список добавленных пользователей или ничего</div>
-            <div>динамически насроенное поле для сообщений</div>
-            <div>кнопка отправки сообщения</div>
+            <div>
+                <textarea onChange={(event) => dispatch(typeNewMessage(event.target.value))} value={message}></textarea>
+            </div>
+            <div>
+                <button onClick={() => dispatch(sendMessage())}>Отправить</button>
+            </div>
+            <div>
+                {uimessages}
+            </div>
           </div>
     )
 }
