b2605ed6a2f24efb5d4cda57161af2b021711491
Ruslan <northernnavy95@gmail.com>
2022-05-16 16:12:53 +0300

    created container to my component

--- mint-network/src/components/Profile/Profile.js
+++ mint-network/src/components/Profile/Profile.js
@@ -3,16 +3,25 @@ import ProfileInfo from "./ProfileInfo";
 import { useSelector } from "react-redux";
 import NewPost from "./NewPost";
 
-const Profile = () => {
-    let status = useSelector((state) => state.profile.profileStatus);
+const Profile = (props) => {
     return (
         <div>
             Banner
-            <ProfileInfo status={status}/>
-            <NewPost />
-
+            <ProfileInfo status={props.status} />
+            <NewPost newValue={props.newValue} posts={props.posts} />
         </div>
     )
 }
 
-export default Profile;
+const ProfileContainer = () => {
+
+    let status = useSelector(state => state.profile.profileStatus);
+    let newValue = useSelector(state => state.profile.newPostText);
+    let posts = useSelector(state => state.profile.posts);
+    
+    return (
+        <Profile status={status} newValue={newValue} posts={posts} />
+    )
+}
+
+export default ProfileContainer;
