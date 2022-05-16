53d5c683ee1e2cf09ac9e49b52b2d9ffc4f8cc18
Ruslan <northernnavy95@gmail.com>
2022-05-16 16:09:34 +0300

    created AppContainer. It wiil give child component some data later, also i created the imitation of logging

--- mint-network/src/App.js
+++ mint-network/src/App.js
@@ -2,13 +2,16 @@ import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Header from './components/Header';
 import Navbar from './components/Navbar';
-import Profile from './components/Profile/Profile';
 import Users from './components/Users';
-import Messages from './components/Messages';
+import MessagesContainer from './components/Messages';
 import Settings from './components/Settings';
+import Login from './components/Login';
+import { useDispatch, useSelector } from 'react-redux';
+import { initializeApp } from './redux/reducers/appReducer';
+import ProfileContainer from './components/Profile/Profile';
+
 
 const App = () => {
-  // все селекторы провести здесь, и далее прокинуть пропсами. это облегчит работу 
   return (
     <BrowserRouter>
       <div id='appWrapper'>
@@ -16,10 +19,11 @@ const App = () => {
         <Navbar />        
         <div id='dynamicSection'>
           <Routes>
-            <Route path='/profile' element={<Profile />} />
+            <Route path='/profile' element={<ProfileContainer />} />
             <Route path='/users' element={<Users />} />
-            <Route path='/messages' element={<Messages />} />
+            <Route path='/messages' element={<MessagesContainer />} />
             <Route path='/settings' element={<Settings />} />
+            <Route path='/login' element={<Login />}/>
           </Routes>
         </div>
       </div>
@@ -27,4 +31,20 @@ const App = () => {
   );
 }
 
-export default App;
+const AppContainer = () => {
+  let initialized = useSelector(state => state.app.initialized);
+  let dispatch = useDispatch();
+  return (
+    <div>
+      {initialized 
+      ? <App />
+      : <div>
+          not auth
+          <button onClick={() => dispatch(initializeApp())}>enter</button>
+        </div>
+      }
+    </div>
+  )
+}
+
+export default AppContainer;
