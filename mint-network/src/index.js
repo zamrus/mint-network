112962fb6e02991c335c7c1c2744bd958584565c
Ruslan <northernnavy95@gmail.com>
2022-05-16 16:07:52 +0300

    now it will render AppContainer

--- mint-network/src/index.js
+++ mint-network/src/index.js
@@ -3,14 +3,15 @@ import ReactDOMClient from 'react-dom/client';
 import { Provider } from 'react-redux';
 import store from './redux/store';
 import reportWebVitals from './reportWebVitals';
-import App from './App';
+import AppContainer from './App';
+
 
 const container = document.getElementById('root');
 const root = ReactDOMClient.createRoot(container);
 
 root.render(
     <Provider store={store}>
-        <App/>
+        <AppContainer />
     </Provider>
 );
 
