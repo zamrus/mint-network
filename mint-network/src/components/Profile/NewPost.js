8952c21d0c92aeb411b7a568e753eb228512fbfb
Ruslan <northernnavy95@gmail.com>
2022-05-16 16:14:02 +0300

    removed selection logic from component, now it will use props

--- mint-network/src/components/Profile/NewPost.js
+++ mint-network/src/components/Profile/NewPost.js
@@ -1,17 +1,16 @@
 import React from "react";
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
 import { addPost, typeNewPost } from "../../redux/reducers/profileReducer";
 
-const NewPost = () => {
+const NewPost = (props) => {
     const dispatch = useDispatch();
-    const newValue = useSelector((state) => state.profile.newPostText);
-    const posts = useSelector((state) => state.profile.posts);
-    let uiposts = posts.map((obj) => {
+   
+    let uiposts = props.posts.map((obj) => {
         return <div>{obj.text}</div>
     })
     return (
         <div>
-            <textarea value={newValue} onChange={(event) => {
+            <textarea value={props.newValue} onChange={(event) => {
                 dispatch(typeNewPost(event.target.value));
             }}></textarea>
             <button onClick={() => dispatch(addPost())}>добавить пост</button>
