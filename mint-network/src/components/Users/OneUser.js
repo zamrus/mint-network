9698d0d94e1f8f36fd56d819465c89aa2772da59
Ruslan <northernnavy95@gmail.com>
2022-05-17 13:57:54 +0300

    created separated user component. It will take parent props and use it for following/unfollowing

---
+++ mint-network/src/components/Users/OneUser.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { useDispatch } from "react-redux";
+import { follow, unfollow } from "../../redux/reducers/usersReducer";
+
+
+const OneUser = (props) => {
+    let dispatch = useDispatch();
+    return (
+        <div>
+            <div>ава</div>
+            <div>{props.name}</div>
+            <div>
+                {props.followed
+                  ? <button onClick={() => dispatch(unfollow(props.id))}>unfollow</button>
+                  : <button onClick={() => dispatch(follow(props.id))}>follow</button>
+                }
+            </div>
+            <div>статус пользователя</div>
+        </div>
+    )
+}
+
+export default OneUser;
