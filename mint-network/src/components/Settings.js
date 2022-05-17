6f54ae70be69d6a5ea472a6925a306f3b6e9fba0
Ruslan <northernnavy95@gmail.com>
2022-05-17 13:55:10 +0300

    added of fun counter using hooks

--- mint-network/src/components/Settings.js
+++ mint-network/src/components/Settings.js
@@ -1,7 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Settings = () => {
-    return <div>Settings info</div>
+
+    let [count, changeCount] = useState(1);
+    let increment = () => {
+        changeCount(count + 1);
+    }
+    let refresh = () => {
+        changeCount(0);
+    }
+    return (
+        <div>
+            <div>{count}</div>
+            <button onClick={() => (increment())}>change count</button>
+            <button onClick={() => (refresh())}>clear counter</button>
+        </div>
+    )
 }
 
 export default Settings;
