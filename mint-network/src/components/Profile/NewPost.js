856def93e8ebd6fa972c4e2ca4da74dc5f449371
Ruslan <northernnavy95@gmail.com>
2022-05-31 13:06:01 +0300

    added changeable posts

--- mint-network/src/components/Profile/NewPost.js
+++ mint-network/src/components/Profile/NewPost.js
@@ -1,24 +1,37 @@
 import React from "react";
 import { useDispatch } from "react-redux";
-import { addPost, deletePost, typeNewPost } from "../../redux/reducers/profileReducer";
+import { addNewPost, deletePost, typeNewPost, setChangingPost, changeCurrentPost, completeChangePost } from "../../redux/reducers/profileReducer";
 
 const NewPost = (props) => {
+    debugger
     const dispatch = useDispatch();
    
     let uiposts = props.posts.map((obj) => {
         return (
             <div>
-                <div>{obj.text}</div>
-                <button onClick={() => dispatch(deletePost(obj.id))}>delete post</button>
+                {obj.changed
+                    ? <div>
+                        <textarea autoFocus value={props.changingValue} onChange={(event) => {
+                            dispatch(changeCurrentPost(event.target.value))
+                        }}></textarea>
+                        <button onClick={() => dispatch(completeChangePost())}>done</button>
+                      </div>
+                    : <div>
+                        <div>{obj.text}</div>
+                        <button onClick={() => dispatch(deletePost(obj.id))}>delete post</button>
+                        <button onClick={() => dispatch(setChangingPost(obj.id))}>change post</button>
+                      </div>
+                } 
+                
             </div>
         )
-    })
+    }) // логика написания исправления уже созданного поста
     return (
         <div>
             <textarea value={props.newValue} onChange={(event) => {
                 dispatch(typeNewPost(event.target.value));
             }}></textarea>
-            <button onClick={() => dispatch(addPost())}>добавить пост</button>
+            <button onClick={() => dispatch(addNewPost())}>добавить пост</button>
             <div>{uiposts}</div>
         </div>
     )
--- mint-network/src/components/Profile/Profile.js
+++ mint-network/src/components/Profile/Profile.js
@@ -8,7 +8,7 @@ const Profile = (props) => {
         <div>
             Banner
             <ProfileInfo status={props.status} />
-            <NewPost newValue={props.newValue} posts={props.posts} />
+            <NewPost newValue={props.newValue} changingValue={props.changingValue} posts={props.posts} />
         </div>
     )
 }
@@ -17,10 +17,11 @@ const ProfileContainer = () => {
 
     let status = useSelector(state => state.profile.profileStatus);
     let newValue = useSelector(state => state.profile.newPostText);
+    let changingValue = useSelector(state => state.profile.changingPostText)
     let posts = useSelector(state => state.profile.posts);
     
     return (
-        <Profile status={status} newValue={newValue} posts={posts} />
+        <Profile status={status} newValue={newValue} changingValue={changingValue} posts={posts} />
     )
 }
 
