1cd624adfb6a0bd2e2cd0706c289afddaefdc6b1
Ruslan <northernnavy95@gmail.com>
2022-11-14 14:23:58 +0300

    added decrement hook and button

--- mint-network/src/components/Settings.js
+++ mint-network/src/components/Settings.js
@@ -3,16 +3,14 @@ import React, { useState } from "react";
 const Settings = () => {
 
     let [count, changeCount] = useState(1);
-    let increment = () => {
-        changeCount(count + 1);
-    }
-    let refresh = () => {
-        changeCount(0);
-    }
+    let increment = () => changeCount(count + 1);
+    let decrement = () => changeCount(count - 1);
+    let refresh = () => changeCount(0);
     return (
         <div>
             <div>{count}</div>
-            <button onClick={() => (increment())}>change count</button>
+            <button onClick={() => (increment())}>count + 1</button>
+            <button onClick={() => (decrement())}>count - 1</button>
             <button onClick={() => (refresh())}>clear counter</button>
         </div>
     )
