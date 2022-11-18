fdcb22b98cb38eab58a7142ea7ca74cb1569bf8e
Ruslan <northernnavy95@gmail.com>
2022-11-18 15:51:19 +0300

    added some simple banner

---
+++ mint-network/src/components/Profile/Banner.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Banner = () => {
+    return (
+        <div>
+            <div>
+                <img src={''} width={''} height={''} /> avatar
+            </div>
+            <div id='background'>
+                <span>friends </span>
+                <span>photos </span>
+                <span>music</span>
+            </div>       
+        </div>
+    )
+}
+
+export default Banner;
