408712967efab8292ed1c6ae2554d59fa80dbf7c
Ruslan <northernnavy95@gmail.com>
2022-11-21 12:04:40 +0300

    testing useEffect and useState, later it will be removed

--- mint-network/src/components/Settings.js
+++ mint-network/src/components/Settings.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 const Settings = () => {
 
@@ -6,12 +6,30 @@ const Settings = () => {
     let increment = () => changeCount(count + 1);
     let decrement = () => changeCount(count - 1);
     let refresh = () => changeCount(0);
+
+    let [isWriting, setIsWriting] = useState(false);
+
+    let startWrite = () => setIsWriting(true);
+    let finishWrite = () => setIsWriting(false);
+
+    useEffect(() => {
+       console.log(`checking useEffect + ${count}`);
+    }, [count]); 
+
     return (
         <div>
             <div>{count}</div>
             <button onClick={() => (increment())}>count + 1</button>
             <button onClick={() => (decrement())}>count - 1</button>
             <button onClick={() => (refresh())}>clear counter</button>
+            {isWriting
+              ? <div>you are writing</div>
+              : <div>you are not writing</div>
+            }
+            <div>
+                <button onClick={startWrite}>write</button>
+                <button onClick={finishWrite}>no write</button>
+            </div>
         </div>
     )
 }
