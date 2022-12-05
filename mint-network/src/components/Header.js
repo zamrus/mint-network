5d73103bc806b0f7a00957d66eb719799d3868a3
Ruslan <northernnavy95@gmail.com>
2022-12-05 21:18:50 +0300

    improved logout callback

--- mint-network/src/components/Header.js
+++ mint-network/src/components/Header.js
@@ -1,16 +1,23 @@
 import React from "react";
 import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
 import { logout } from "../redux/reducers/appReducer";  
 
 
 const Header = (props) => {
   let dispatch = useDispatch();
+  let navigate = useNavigate();
+
+  let byeBye = () => {
+    dispatch(logout());
+    navigate('/login');
+  }
     return (
       <header>
         <div>LOGO</div>
         <div>
           <span>{props.nickName}</span>
-          <button onClick={() => dispatch(logout())}>logout</button>
+          <button onClick={byeBye}>logout</button>
         </div>
       </header>
     )
