23a247635af753523524b7ecdfb25eb71b89d56f
Ruslan <northernnavy95@gmail.com>
2022-12-06 15:32:44 +0300

    refactored big issue - tapping new status now is on local state control

--- mint-network/src/components/Profile/ProfileStatus.js
+++ mint-network/src/components/Profile/ProfileStatus.js
@@ -9,29 +9,24 @@ const ProfileStatus = (props) => {
 
     let [isTyping, setIsTyping] = useState(false); // локально меняет строку на поле ввода и обратно
 
+    let [currentStatus, changeStatus] = useState(props.status); // принимает через пропсы текст из стора, создает функцию посимвольного ввода в поле 
+
     let activateTyping = () => setIsTyping(true); // меняет локальный стейт, дает возможность печатать в поле ввода
 
     let deactivateTyping = () => { // возвращает строку, отправляет напечатанный текст через экшен в редьюсер
         setIsTyping(false);
         dispatch(addStatus(currentStatus)); // currentStatus это текущий текст, набранный пользователем
     }
-
-    let [currentStatus, changeStatus] = useState(props.status); // принимает через пропсы текст из стора, создает функцию посимвольного ввода в поле 
     
-    let onStatusChange = (event) => { // функция обработчик, берущая из события каждый введенный символ и передающая их в функцию хука
-        changeStatus(event.currentTarget.value);
-    }
-
     useEffect(() => {
-        changeStatus(props.status);
-        console.log('data was changed');
+        changeStatus(props.status);  // при загрузке страницы помещает текст статуса из пропсов в локальный стейт 
+        console.log('set status from props to local state');
     }, [props.status])
 
     return (
         <div>
-            <div>{`current data is ${props.status}`}</div>
             {isTyping 
-                ? <input type={'text'} autoFocus onChange={onStatusChange} onBlur={deactivateTyping} value={currentStatus} />
+                ? <input type={'text'} autoFocus onChange={(e) => changeStatus(e.target.value)} onBlur={deactivateTyping} value={currentStatus} />
                 : <span onClick={activateTyping}>{props.status || 'enter your status'}</span>
             }
         </div>
