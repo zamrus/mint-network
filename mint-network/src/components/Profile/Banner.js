337d24103e9ac17cbb5984618365a058121907fd
Ruslan <northernnavy95@gmail.com>
2022-12-06 15:34:37 +0300

    added simple button with redirect to users with users count

--- mint-network/src/components/Profile/Banner.js
+++ mint-network/src/components/Profile/Banner.js
@@ -1,13 +1,21 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
-const Banner = () => {
+const Banner = (props) => {
+
+    let navigate = useNavigate();
+
+    let toUsers = () => {
+        navigate('/users')
+    }
+
     return (
         <div>
             <div>
                 <img src={''} width={''} height={''} /> avatar
             </div>
             <div id='background'>
-                <span>friends </span>
+                <button onClick={toUsers}>users: {props.users}</button>
                 <span>photos </span>
                 <span>music</span>
             </div>       
